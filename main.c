ba98d0887c5d474d2773ad64235f9565ce68172b
Alen Yu <lenyu@ucdavis.edu>
2018-03-07 21:54:14 -0800

    Skeleton functions and structs

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Tweeter {
+    char * name;
+    int tweetCount;
+};
+
+int getFieldPos(char * fieldName) {
+    int pos = -1;
+    return pos;
+}
+
+int getField(int pos) {
+
+}
+
+int main(int argc, char ** argv) {
+
+    printf("%s", "Hello World\n\0");
+
+}
