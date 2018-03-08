c344d3fa1f0f7af8bec8d2a5c357cf3a62d152cf
Alen Yu <lenyu@ucdavis.edu>
2018-03-07 22:49:20 -0800

    Added implementation for getFieldPos to get integer position of field name

--- main.c
+++ main.c
@@ -7,16 +7,39 @@ struct Tweeter {
     int tweetCount;
 };
 
-int getFieldPos(char * fieldName) {
+int getFieldPos(char * fieldName, char * header) {
+
     int pos = -1;
+    int count = 0;
+    char * token = strtok(header,",");
+    while(token != NULL) {
+        //printf("%s %s\n", token, fieldName);
+        //printf("Compare: %d\n", strcmp(token, fieldName ));
+        if( strcmp(token, fieldName ) == 0)
+            pos = count;
+        token = strtok(NULL, ",");
+        count++;
+    }
     return pos;
+
 }
 
-int getField(int pos) {
+int getField(int pos, char * line) {
 
 }
 
 int main(int argc, char ** argv) {
+
+    char * fileName = argv[1];
+    FILE * ofstream = fopen(fileName, "r");
+
+    if(ofstream == NULL) {
+        printf("File does not exist\n\0");
+        exit(0);
+    }
+    char line[1024];
+    fgets(line, 1024, ofstream);
+    printf("%d\n", getFieldPos("Hello world", line));
 
     printf("%s", "Hello World\n\0");
 
