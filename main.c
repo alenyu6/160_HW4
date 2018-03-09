647045114abb452ffef26f6c6fddd00377949faa
Pavel Kuzkin <pavelkuzkin@Pasha-Mac2017.local>
2018-03-08 20:47:41 -0800

    added map, printMap func, add to map func

--- main.c
+++ main.c
@@ -35,22 +35,61 @@ char * getField(int pos, char * line) {
     return NULL;
 }
 
+void printMap(struct Tweeter* map, int tCount){
+  printf("\n - > map:\n");
+  for (int i = 0; i<tCount; i++){
+    printf("#%d: <%s,%d>\n", i, map[i].name, map[i].tweetCount);
+  }
+  printf("-----------------\n");
+}
+
+int add2Map(struct Tweeter* map, char* username, int twCount){
+  if (map == NULL) return -1;
+
+  for (int i = 0; i<twCount-1; i++){
+    //printf("map[%d]: \n", i);
+    if( strcmp(username, map[i].name ) == 0){
+        //printf(" -> match at %d, %s=%s\n", i, map[i].name, username);
+        map[i].name = strdup(username);
+        map[i].tweetCount++;
+        //printf("adding=%d <%s,%d>\n", i, map[i].name, map[i].tweetCount);
+        return 1;
+    }
+  }
+  map[twCount].name = strdup(username);
+  map[twCount].tweetCount = 1;
+  //printf("adding=%d <%s,%d>\n", twCount, map[twCount].name, map[twCount].tweetCount);
+  //twCount++;
+  return 0;
+}
+
+
+
 int main(int argc, char ** argv) {
 
     char * fileName = argv[1];
     FILE * ofstream = fopen(fileName, "r");
 
+    struct Tweeter twVolume[20000];
+    int twCount = 0;
+
     if(ofstream == NULL) {
         printf("File does not exist\n\0");
         exit(0);
     }
+
     char line[1024];
     fgets(line, 1024, ofstream);
     int namePos = getFieldPos("\"name\"", line);
     printf("%d\n", namePos);
+
     while(fgets(line, 1024, ofstream)) {
-        printf("%s\n", getField(namePos, line));
+        //printf("%s\n", getField(namePos, line));
+        if (add2Map(twVolume, getField(namePos, line), twCount) == 0){
+          twCount++;
+        }
     }
+    printMap(twVolume, twCount);
 
     //printf("%s", "Hello World\n\0");
 
